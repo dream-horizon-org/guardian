33d8a2edb100d1561b861f9b7812bc68228793bd
Koushik Akula <koushik.akula@dream11.com>
2025-12-09 19:45:46 +0530

    user config api

---
+++ src/main/java/com/dreamsportslabs/guardian/dao/UserConfigDao.java
@@ -0,0 +1,60 @@
+package com.dreamsportslabs.guardian.dao;
+
+import static com.dreamsportslabs.guardian.dao.query.UserConfigQuery.GET_USER_CONFIG;
+import static com.dreamsportslabs.guardian.dao.query.UserConfigQuery.UPDATE_USER_CONFIG;
+import static com.dreamsportslabs.guardian.exception.ErrorEnum.INTERNAL_SERVER_ERROR;
+
+import com.dreamsportslabs.guardian.client.MysqlClient;
+import com.dreamsportslabs.guardian.dao.model.UserConfigModel;
+import com.dreamsportslabs.guardian.utils.JsonUtils;
+import com.google.inject.Inject;
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Maybe;
+import io.vertx.rxjava3.sqlclient.Tuple;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor(onConstructor = @__({@Inject}))
+public class UserConfigDao {
+  private final MysqlClient mysqlClient;
+
+  public Maybe<UserConfigModel> getUserConfig(String tenantId) {
+    return mysqlClient
+        .getReaderPool()
+        .preparedQuery(GET_USER_CONFIG)
+        .rxExecute(Tuple.of(tenantId))
+        .flatMapMaybe(
+            result -> {
+              if (result.size() == 0) {
+                return Maybe.empty();
+              }
+              return Maybe.just(JsonUtils.rowSetToList(result, UserConfigModel.class).get(0));
+            })
+        .onErrorResumeNext(err -> Maybe.error(INTERNAL_SERVER_ERROR.getException(err)));
+  }
+
+  public Completable updateUserConfig(UserConfigModel userConfig) {
+    Tuple params =
+        Tuple.tuple()
+            .addBoolean(userConfig.getIsSslEnabled() != null ? userConfig.getIsSslEnabled() : false)
+            .addString(userConfig.getHost())
+            .addInteger(userConfig.getPort())
+            .addString(userConfig.getGetUserPath())
+            .addString(userConfig.getCreateUserPath())
+            .addString(userConfig.getAuthenticateUserPath())
+            .addString(userConfig.getAddProviderPath())
+            .addBoolean(
+                userConfig.getSendProviderDetails() != null
+                    ? userConfig.getSendProviderDetails()
+                    : false)
+            .addString(userConfig.getTenantId());
+
+    return mysqlClient
+        .getWriterPool()
+        .preparedQuery(UPDATE_USER_CONFIG)
+        .rxExecute(params)
+        .ignoreElement()
+        .onErrorResumeNext(err -> Completable.error(INTERNAL_SERVER_ERROR.getException(err)));
+  }
+}
