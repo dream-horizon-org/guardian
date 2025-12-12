a56831713b2b95e4016351342cf6772d8683af0d
Koushik Akula <koushik.akula@dream11.com>
2025-12-12 17:42:20 +0530

    feat: sms config api

---
+++ src/main/java/com/dreamsportslabs/guardian/dao/model/SmsConfigModel.java
@@ -0,0 +1,33 @@
+package com.dreamsportslabs.guardian.dao.model;
+
+import static com.dreamsportslabs.guardian.constant.Constants.DEFAULT_IS_SSL_ENABLED;
+import static com.dreamsportslabs.guardian.constant.Constants.DEFAULT_SMS_CONFIG_PORT;
+import static com.dreamsportslabs.guardian.utils.Utils.JsonToStringDeserializer;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.jackson.Jacksonized;
+
+@Getter
+@Builder
+@Setter
+@Jacksonized
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class SmsConfigModel {
+  private String tenantId;
+
+  @Builder.Default private Boolean isSslEnabled = DEFAULT_IS_SSL_ENABLED;
+
+  private String host;
+
+  @Builder.Default private Integer port = DEFAULT_SMS_CONFIG_PORT;
+
+  private String sendSmsPath;
+  private String templateName;
+
+  @JsonDeserialize(using = JsonToStringDeserializer.class)
+  private String templateParams;
+}
--- src/test/java/com/dreamsportslabs/guardian/utils/ApplicationIoUtils.java
+++ src/test/java/com/dreamsportslabs/guardian/utils/ApplicationIoUtils.java
@@ -683,4 +683,31 @@ public class ApplicationIoUtils {
     return execute(
         null, headers, new HashMap<>(), spec -> spec.delete("/v1/admin/config/email-config"));
   }
+
+  public static Response createSmsConfig(String tenantId, Map<String, Object> body) {
+    Map<String, String> headers = new HashMap<>();
+    headers.put(HEADER_TENANT_ID, tenantId);
+    return execute(
+        body, headers, new HashMap<>(), spec -> spec.post("/v1/admin/config/sms-config"));
+  }
+
+  public static Response getSmsConfig(String tenantId) {
+    Map<String, String> headers = new HashMap<>();
+    headers.put(HEADER_TENANT_ID, tenantId);
+    return execute(null, headers, new HashMap<>(), spec -> spec.get("/v1/admin/config/sms-config"));
+  }
+
+  public static Response updateSmsConfig(String tenantId, Map<String, Object> body) {
+    Map<String, String> headers = new HashMap<>();
+    headers.put(HEADER_TENANT_ID, tenantId);
+    return execute(
+        body, headers, new HashMap<>(), spec -> spec.patch("/v1/admin/config/sms-config"));
+  }
+
+  public static Response deleteSmsConfig(String tenantId) {
+    Map<String, String> headers = new HashMap<>();
+    headers.put(HEADER_TENANT_ID, tenantId);
+    return execute(
+        null, headers, new HashMap<>(), spec -> spec.delete("/v1/admin/config/sms-config"));
+  }
 }
